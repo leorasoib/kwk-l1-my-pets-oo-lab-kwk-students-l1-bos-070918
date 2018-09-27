79488d51a0c17fedcba361467c822099c4e817fa
Leora Soibelman <21lsoibelman@gannacademy.org>
2018-09-27 14:40:39 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,8 +1,7 @@
 class Cat
 <<<<<<< HEAD
   attr_accessor :mood
-=======
->>>>>>> f241659838b3bf9e4cd677716a940bd04679e2e2
+
   def initialize(cat)
     @cat = cat
     @name = "crookshanks"
@@ -11,10 +10,7 @@ class Cat
   def name 
     @name = "crookshanks"
   end 
-<<<<<<< HEAD
-  
 
-=======
   def mood
     @mood = mood
   end 
